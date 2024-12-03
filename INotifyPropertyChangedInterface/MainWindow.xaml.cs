d85f2078186e9a3542cafcc95a350c37c54e28d8
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-12-03 14:57:35 +0100

    Projektdateien hinzufügen.

---
+++ INotifyPropertyChangedInterface/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INotifyPropertyChangedInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Summe SummeObjekt { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // damit sagt man das der DatenKontext unsere 'MainWindow' das 'SummeObjekt' ist. 
+            SummeObjekt = new Summe { Nummer1= "1", Nummer2="3" };
+            this.DataContext = SummeObjekt;
+        }
+
+    }
+}
---
+++ INotifyPropertyChangedInterface/Summe.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+/*
+ Was macht das?
+  public event PropertyChangedEventHandler? PropertyChanged; 
+
+es wartet bis sich eine property verändert und es getriggert wird! um etwas auszuführen.
+ */
+namespace INotifyPropertyChangedInterface
+{
+    public class Summe : INotifyPropertyChanged
+    {
+
+        private string nummer1;
+        private string nummer2;
+        private string ergebnis;
+
+        public string Nummer1
+        {
+            get { return nummer1; }
+            set
+            {
+                int nummer;
+                bool res = int.TryParse(value, out nummer); // Konvertiert den string in ein Int32
+                if (res) nummer1 = value;      // wenn eine veränderung am 'nummer1' ausgeführt wird, soll auch das Property 'Nummer1' angepasst wedren
+                OnPropertyChanged("Nummer1");
+                OnPropertyChanged("Ergebnis"); // immer wenn man die Zahl 1 verändert, verändert sich ja auch das ergebnisat!
+            }
+
+        }
+
+        public string Nummer2
+        {
+            get { return nummer2; }
+            set
+            {
+                int nummer;
+                bool res = int.TryParse(value, out nummer); // Konvertiert den string in ein Int32
+                if (res) nummer2 = value;      // wenn eine veränderung am 'nummer2' ausgeführt wird, soll auch das Property 'Nummer2' angepasst werden
+                OnPropertyChanged("Nummer2");
+                OnPropertyChanged("Ergebnis"); // immer wenn man die Zahl 2 verändert, verändert sich ja auch das ergebnisat!
+            }
+
+        }
+
+        public string Ergebnis
+        {
+            get 
+            { 
+                int res = int.Parse(Nummer1) + int.Parse(Nummer2); // wird erst vom string zum int und dann wieder zum string umgewandelt
+                return res.ToString();
+            }
+            set
+            {
+                int res = int.Parse(Nummer1) + int.Parse(Nummer2); // wird erst vom string zum int und dann wieder zum string umgewandelt
+                ergebnis = res.ToString();      // wenn eine veränderung am 'nummer1' ausgeführt wird, soll auch das Property 'Nummer1' angepasst wedren
+                OnPropertyChanged("Ergebnis");
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; 
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null) // der code soll nur ausgeführt wedredn wenn 'PropertyChanged' nicht 'null' ist!
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
